b0c9471764c3fe44642be1dc5780ce267b84dfe5
bautchc <bautchc@msoe.edu>
2023-11-17 19:38:31 -0600

    Cleaned up spacing

--- lib/curve.rb
+++ lib/curve.rb
@@ -64,9 +64,10 @@ class Curve
                   .filter { |star| @settings[star] }
                   .map { |star| [star, (@settings[star] * reviews).round] }
     curve.append([5, reviews - curve.map(&:last).sum])
-    curve.reverse.each do |star, count| 
-      puts "#{star.to_s.ljust(curve.map { |star, _| star.to_s.length}.max)} : #{count}"
-    end
+    curve.reverse
+         .each do |star, count| 
+           puts "#{star.to_s.ljust(curve.map { |star, _| star.to_s.length}.max)} : #{count}"
+         end
     curve.to_h
   end
 
